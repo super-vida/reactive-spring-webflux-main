436cc437a313a40602792ef8354c47f42fd74b61
vacek <vacek.honza@gmail.com>
2022-09-28 22:54:49 +0200

    next commit

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,19 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+        StepVerifier.create(namesFlux).expectNext("alex", "ben", "chloe").verifyComplete();
+
+    }
 }
