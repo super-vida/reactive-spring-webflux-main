b2c3b07e3a2ac350b03616d5fd96d8a1f6c467c1
vacek <vacek.honza@gmail.com>
2022-09-29 23:03:08 +0200

    sync

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -3,7 +3,9 @@ package com.learnreactiveprogramming.service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.function.Function;
 
 public class FluxAndMonoGeneratorService {
 
@@ -28,9 +30,31 @@ public class FluxAndMonoGeneratorService {
                 .log();
     }
 
+    public Flux<String> namesFlux_flatmap_async(int stringLength){
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .flatMap( s -> splitString_async(s))
+                .log();
+    }
+    public Flux<String> namesFlux_concatmap_async(int stringLength){
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .concatMap( s -> splitString_async(s))
+                .log();
+    }
+
     public Flux<String> splitString(String name){
         var charArray = name.split("");
         return  Flux.fromArray(charArray);
+    }
+
+    public Flux<String> splitString_async(String name){
+        var charArray = name.split("");
+        //var delay = new Random().nextInt(1000);
+        var delay = 1000;
+        return  Flux.fromArray(charArray).delayElements(Duration.ofMillis(delay));
     }
 
     public Flux<String> namesFlux_immutable(){
@@ -43,6 +67,60 @@ public class FluxAndMonoGeneratorService {
     public Mono nameMono(){
         return Mono.just("alex").log();
     }
+
+    public Mono<List<String>> nameMono_flatMap(int stringLength){
+        return Mono.just("alex")
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .flatMap(this::splitStringMono)
+                .log();
+    }
+
+    public Flux<String> nameMono_flatMapMany(int stringLength){
+        return Mono.just("alex")
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .flatMapMany(this::splitString)
+                .log();
+    }
+
+    private Mono<List<String>> splitStringMono(String s) {
+        var charArray = s.split("");
+        var listArray = List.of(charArray);
+        return Mono.just(listArray);
+    }
+
+    public Flux<String> namesFlux_transform(int stringLength){
+
+        Function <Flux<String>, Flux<String>> filterMap = name -> name.map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength);
+
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .transform(filterMap)
+                .flatMap( s -> splitString(s))
+                .defaultIfEmpty("default")
+                .log();
+    }
+
+    public Flux<String> namesFlux_transform_switchIfEmpty(int stringLength){
+
+        Function <Flux<String>, Flux<String>> filterMap = name -> name.map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .flatMap( s -> splitString(s));
+
+        var defaultFlux = Flux.just("default")
+                .transform(filterMap);
+
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .transform(filterMap)
+                .flatMap( s -> splitString(s))
+                .switchIfEmpty(defaultFlux)
+                .log();
+    }
+
+
+
+
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
@@ -50,6 +52,71 @@ class FluxAndMonoGeneratorServiceTest {
 		var namesFlux = fluxAndMonoGeneratorService.namesFlux_flatmap(stringLength);
 		StepVerifier.create(namesFlux)
 				.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+				.verifyComplete();
+	}
+
+	@Test
+	void namesFlux_flatmap_async() {
+		int stringLength = 3;
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux_flatmap_async(stringLength);
+		StepVerifier.create(namesFlux)
+				//.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+				.expectNextCount(9)
+				.verifyComplete();
+	}
+	@Test
+	void namesFlux_concatmap_async() {
+		int stringLength = 3;
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux_concatmap_async(stringLength);
+		StepVerifier.create(namesFlux)
+				.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+				.verifyComplete();
+	}
+
+	@Test
+	void nameMono_flatMap() {
+		int stringLength = 3;
+		var namesFlux = fluxAndMonoGeneratorService.nameMono_flatMap(stringLength);
+		StepVerifier.create(namesFlux)
+				.expectNext(List.of("A", "L", "E", "X"))
+				.verifyComplete();
+	}
+
+	@Test
+	void nameMono_flatMapMany() {
+		int stringLength = 3;
+		var namesFlux = fluxAndMonoGeneratorService.nameMono_flatMapMany(stringLength);
+		StepVerifier.create(namesFlux)
+				.expectNext("A", "L", "E", "X")
+				.verifyComplete();
+	}
+
+    @Test
+    void namesFlux_transform() {
+		int stringLength = 3;
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux_transform(stringLength);
+		StepVerifier.create(namesFlux)
+				.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+				.verifyComplete();
+    }
+
+	@Test
+	void namesFlux_transform_1() {
+		int stringLength = 6;
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux_transform(stringLength);
+		StepVerifier.create(namesFlux)
+				//.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+				.expectNext("default")
+				.verifyComplete();
+	}
+
+	@Test
+	void namesFlux_transform_swithIfEmpty() {
+		int stringLength = 6;
+		var namesFlux = fluxAndMonoGeneratorService.namesFlux_transform_switchIfEmpty(stringLength);
+		StepVerifier.create(namesFlux)
+				//.expectNext("A", "L", "E", "X", "C", "H", "L", "O", "E")
+				.expectNext("D","E","F","A","U","L","T")
 				.verifyComplete();
 	}
 }
